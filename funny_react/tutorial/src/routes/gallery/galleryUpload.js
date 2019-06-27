2048fdf15a8c386b07cb718f6e9df2241e55acfd
echo <off.echoi@gmail.com>
2019-06-27 20:58:45 +0900

    gall

---
+++ funny_react/tutorial/src/routes/gallery/galleryUpload.js
@@ -0,0 +1,11 @@
+import React, { Component ,Fragment } from 'react';
+
+class galleryUpload extends Component {
+    render() {
+        return (
+                <input type="file"/>
+        );
+    }
+}
+
+export default galleryUpload;
