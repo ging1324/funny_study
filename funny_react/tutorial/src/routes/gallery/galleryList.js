f5f409224b4dc1153cd95acc8216a979c8f46264
echo <off.echoi@gmail.com>
2019-07-01 20:32:50 +0900

    gallery Pop

--- funny_react/tutorial/src/routes/gallery/galleryList.js
+++ funny_react/tutorial/src/routes/gallery/galleryList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link,Route } from 'react-router-dom'
 import GalleryUpload from './galleryUpload'
+import GalleryView from './galleryView'
 
 // 이미지 경로는 ./를 기준으로 public에 설정되어 있음!  public에 image 폴더 넣기 (Vue 는 asset)
  
@@ -79,6 +80,29 @@ const photos = [
 // ];
 
 class galleryList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showPopup : false
+            // popup 안뜨는 상태
+        }
+    }
+    // togglePop = () => {
+    //     this.setState({
+    //         // setState는 state에 변화를 주기 위해서 무조건 거쳐야함 = state에 변화를 주기위해 사용
+    //         //객체로 전달된는 값만 업데이트
+    //         showPopup : !this.state.showPopup
+    //     })
+    // }
+    togglePop = () => {
+        this.setState(
+            (state)=>({
+                // 위의 코드랑 같은데, setState에 state를 받아오면 내부에서 this.state를 안 써주어도 됨
+                showPopup : !state.showPopup
+            })
+        )
+    }
+
     render() {
         return (
             <div>
@@ -87,10 +111,16 @@ class galleryList extends Component {
                         <GalleryUpload/>
                     </li>
                     {photos.map(photo => {
-                        return <li><Link to="/gallery/galleryView"><img title={photo.title} src={photo.pimages}/></Link></li>
+                        return <li><button onClick={this.togglePop.bind(this)}><img title={photo.title} src={photo.pimages}/></button></li>
+                        // bind(this) 가 뭐지?? this ?? 여기 컴포넌트 내의 togglePop 함수?
                     })}
                 </ul>
                 {/* <PhotoList psrc={psrc} /> */}
+                {this.state.showPopup ? 
+                    <GalleryView closePopup={this.togglePop.bind(this)} />
+                    //  ?? 닫는것...? // props 로 함수를 받아서 닫힘 버튼에 넣어줄 수 있나?
+                    : null
+                }
             </div>
         );
     }
--- funny_react/tutorial/src/routes/gallery/galleryView.js
+++ funny_react/tutorial/src/routes/gallery/galleryView.js
@@ -5,7 +5,8 @@ class galleryView extends Component {
     render() {
         return (
             <div>
-                <div className="fixWrap">
+                <div className="fixWrap" onClick={(e)=>{this.props.closePopup(); e.stopPropagation();}} >
+                    {/* togglePop을 여기서 closePop으로 받아주는건가? */}
                     <div className="imgWrap">
                         <img src="./image/a1.jpg"/>
                     </div>
