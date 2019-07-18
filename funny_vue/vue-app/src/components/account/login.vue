d87010f18d3666a2cc6d5dee9ca934ca239a7717
ging1324 <ging1324@gmail.com>
2019-07-18 11:32:37 +0900

    update to sns for vue

--- funny_vue/vue-app/src/components/account/login.vue
+++ funny_vue/vue-app/src/components/account/login.vue
@@ -11,14 +11,48 @@
             </div>
             <button @click="login">로그인</button>
         </ul>
+        <KakaoLogin
+        api-key="10b8a3e39ea26078e582d767275366c5"
+        image="kakao_login_btn_small"
+        :on-success=successKakao
+        :on-failure=failSns
+        />
+        <GoogleLogin 
+        :params="params"
+        :onSuccess=successGoogle
+        :onFailure=failSns
+        :renderParams="renderParams">login</GoogleLogin>
     </div>
 </template>
 <script>
+import KakaoLogin from 'vue-kakao-login'
+import GoogleLogin from 'vue-google-login'
+// let onSuccess = (data) => {
+//   console.log(data)
+//   console.log("success")
+// }
+// let onFailure = (data) => {
+//   console.log(data)
+//   console.log("failure")
+// }
 export default {
+    components: {
+    KakaoLogin,
+    GoogleLogin
+  },
   data(){
       return {
           uid:'',
-          upwd:''
+          upwd:'',
+          params:{
+              client_id: '1028572689384-kq3n2vtj88451on6ct2lc8elcdn9ijlr.apps.googleusercontent.com',
+              scope: 'profile'
+          },
+          renderParams: {
+                    width: 250,
+                    height: 50,
+                    longtitle: true
+                }
       }
   },
   methods:{
@@ -34,7 +68,42 @@ export default {
                 this.$router.push('/');
             }
           })
+      },
+      async successKakao(data){
+          console.info('kakao------------------', data);
+        const userInfo = await this.axios({
+            methods: 'get',
+            url: 'https://kapi.kakao.com/v2/user/me',
+            headers: {
+                'Conetne-type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'Authorization': 'Bearer '+ data.access_token
+            }
+        })
+        console.log(userInfo);
+        this.reqUserLoginForSns('kakao', userInfo.data.id)
+
+      },
+      failSns(data){
+          console.info('failSns-------------------', data);
+      },
+      successGoogle(data){
+          console.info('google------------------', data);
+      },
+      reqUserLoginForSns(type, key){
+          this.axios.get('http://ec2-54-180-31-0.ap-northeast-2.compute.amazonaws.com:3005/study/loginForSns?sns_type='+type+'&sns_key='+key)
+          .then((res) => {
+            //   this.getTest = res.data.result.user_name
+            //   console.log('res', res)
+            if(res.status === 200){
+                alert('로그인에 성공하였습니다!!')
+                localStorage.setItem('is_login', true)
+                localStorage.setItem('login_info', JSON.stringify(res.data.result))
+                this.$router.push('/');
+            }
+          })
       }
+        // onSuccess,
+        // onFailure
   }
 }
 </script>
