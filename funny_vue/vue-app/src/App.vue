3ea56e304e4f9d3e12b7be8037f43d03974afb54
joabyjoa <joabyjoa@gmail.com>
2019-07-25 16:33:04 +0900

    Styling header

--- funny_vue/vue-app/src/App.vue
+++ funny_vue/vue-app/src/App.vue
@@ -1,15 +1,20 @@
 <template>
     <div id="app">
-        {{loginName}}
-        <br/>
-        <router-link to="/home">홈 </router-link> 
-        <router-link to="/login">로그인 </router-link> 
-        <router-link to="/join">회원가입 </router-link> 
-        <router-link to="/boardList">게시판 </router-link>
-        <button v-if="loginStatus" @click="logout">
-            로그아웃
-        </button>
-        <router-view></router-view>
+        <header class="header">
+            <nav class="navigation">
+                <router-link to="/home">홈 </router-link> 
+                <router-link to="/login">로그인 </router-link> 
+                <router-link to="/join">회원가입 </router-link> 
+                <router-link to="/boardList">게시판 </router-link>
+            </nav>
+            <div class="user">
+                <p>{{ loginName }}</p>
+                <button v-if="loginStatus" @click="logout">
+                    로그아웃
+                </button>
+            </div>
+        </header>
+        <router-view class="view"></router-view>
     </div>
 </template>
 
@@ -41,12 +46,60 @@ export default {
 </script>
 
 <style>
-#app {
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+ul, ol{
+    list-style: none;
+}
+button, a{
+    all: unset
+}
+button, a{
+    cursor: pointer;
+}
+button{
+    padding: .3rem 1rem !important;
+    border-radius: 7px;
+    border: 1px solid dodgerblue;
+    background: #fff;
+    color: dodgerblue;
+    font-weight: 700;
+}
+.router-link-exact-active{
+    font-weight: bold;
+}
+
+.header,
+.header .navigation,
+.header .user{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header{
+    padding: .5rem 0;
+    background: dodgerblue;
+    color: #fff;
+    /* font-weight: 700; */
+}
+.header .navigation > *,
+.header .user > *{
+    margin: 0 1rem;
+}
+.header button{
+    all: unset;
+}
+.header + .view{
+    padding: 1rem;
 }
 </style>
